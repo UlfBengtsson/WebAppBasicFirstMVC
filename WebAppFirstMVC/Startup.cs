a21db03f57947088b07b743dd6bd9c6b99b11890
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2020-11-12 11:39:22 +0100

    how to get BrowserLink to work

--- WebAppFirstMVC/Startup.cs
+++ WebAppFirstMVC/Startup.cs
@@ -16,8 +16,14 @@ namespace WebAppFirstMVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();//enable ous to use MVC
             //services.AddControllersWithViews();
+            services.AddMvc()//enable ous to use MVC
+                .AddRazorRuntimeCompilation();//needed if you want to use and use Browserlink & one more line of code in the Configure method below.
+                                              // also need to install two NuGet packages
+                                              // 1: Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
+                                              //    controll its the right version 3.1.10 for a Core 3.1 MVC project.
+                                              // 2: Microsoft.VisualStudio.Web.BrowserLink
+                                              //    controll its the right version 2.2.0 for a Core 3.1 MVC project.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +32,7 @@ namespace WebAppFirstMVC
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();//needed if you want to use and use Browserlink & one more line of code in the ConfigureServices method above.
             }
 
             app.UseStaticFiles();//opens up access to the wwwroot folder so we can use our files there like css/javascript/images...
